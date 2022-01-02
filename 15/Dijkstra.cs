66d3bf09e81ee72a87a24063272694c019f1a934
Jakub Findura <findurajakub@gmail.com>
2022-01-02 11:36:54 +0100

    15th of December

---
+++ 15/Dijkstra.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    class Dijkstra
+    {
+        private readonly int[] matrix;
+        private readonly int x;
+        private readonly int y;
+
+        public Dijkstra(int[] matrix, int x, int y)
+        {
+            this.matrix = matrix;
+            this.x = x;
+            this.y = y;
+        }
+
+        public (int[] distance, int[] prev) FindRoute(int source, int target)
+        {
+            var set = new List<int>();
+
+            var distance = new int[x * y];
+            var prev = new int[x * y];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                distance[i] = int.MaxValue;
+                prev[i] = -1;
+                set.Add(i);
+            }
+
+            distance[source] = 0;
+
+            int j = 0;
+
+            while (set.Count > 0)
+            {
+                var current = set.OrderBy(s => distance[s]).First();
+                Console.WriteLine($"Checking #{j++}/{x * y}, {current}");
+
+                set.Remove(current);
+
+                if (current == target)
+                    break;
+
+                var neighbors = GetNeighbors(current);
+                foreach (var n in neighbors)
+                {
+                    var newDist = distance[current] + matrix[n];
+                    if (newDist < distance[n])
+                    {
+                        distance[n] = newDist;
+                        prev[n] = current;
+                    }
+
+                }
+            }
+
+            return (distance, prev);
+        }
+
+        public int[] GetNeighbors(int current)
+        {
+            var n = new List<int>();
+
+            if (current > x - 1) n.Add(current - x);
+            if (current < x * y - x) n.Add(current + x);
+            if (current % x > 0) n.Add(current - 1);
+            if (current % x < x - 1) n.Add(current + 1);
+
+            return n.ToArray();
+        }
+    }
+}
---
+++ 15/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _15
+{
+    class Program
+    {
+        const string FILE = "input.txt";
+
+        static void Main(string[] args)
+        {
+            //First();
+
+            Second();
+        }
+
+        private static int Inc(int n, int i)
+        {
+            var x = (n + i) % 10;
+            if ((n+i) / 10 > 0) x = x + 1;
+            return x;
+        }
+
+        private static void Second()
+        {
+            var fileLines = File.ReadAllLines(FILE).ToList();
+
+            var withRows = fileLines.SelectMany(line => Enumerable.Range(0, 5).SelectMany(i => line.Select(c => int.Parse(c.ToString())).Select(n => Inc(n, i))).ToArray());
+
+            var matrix = Enumerable.Range(0, 5).Select(i => withRows.Select(n => Inc(n, i)).ToArray()).SelectMany(r => r).ToArray();
+
+            var xSize = fileLines.First().Length * 5;
+            var ySize = fileLines.Count * 5;
+
+            //for (int i = 0; i < ySize; i++)
+            //{
+            //    for (int j = 0; j < xSize; j++)
+            //    {
+            //        Console.Write(matrix[j + i * xSize]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine();
+
+            var d = new Dijkstra(matrix, xSize, ySize);
+
+            var res = d.FindRoute(0, xSize * ySize);
+
+            Console.WriteLine($"Risk to get to the target is: {res.distance[xSize * ySize - 1]}");
+        }
+
+        private static void First()
+        {
+            var fileLines = File.ReadAllLines(FILE).ToList();
+
+            var matrix = fileLines.SelectMany(l => l.Select(c => int.Parse(c.ToString()))).ToArray();
+
+            var xSize = fileLines.First().Length;
+            var ySize = fileLines.Count;
+
+            var d = new Dijkstra(matrix, xSize, ySize);
+
+            var res = d.FindRoute(0, xSize * ySize);
+
+            Console.WriteLine($"Risk to get to the target is: {res.distance[xSize * ySize - 1]}");
+        }
+    }
+}
