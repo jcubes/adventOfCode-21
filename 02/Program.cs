c3e4d80f9c90b499129d470472c1530d1b85c0a9
Jakub Findura <findurajakub@gmail.com>
2022-01-02 11:31:27 +0100

    2nd of December

---
+++ 02/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Second();
+        }
+
+        private static void Second()
+        {
+            int[] position = new int[2];
+
+            int aim = 0;
+
+            foreach (string line in File.ReadLines(@"input.txt"))
+            {
+                var command = line.Split(" ")[0];
+                var move = int.Parse(line.Split(" ")[1]);
+
+                if (command.Equals("forward"))
+                {
+                    position[0] += move;
+                    position[1] += move * aim;
+                }
+
+                if (command.Equals("down"))
+                    aim += move;
+
+                if (command.Equals("up"))
+                    aim -= move;
+            }
+
+            Console.WriteLine($"Final position is {position[0]} with depth {position[1]}. Multiplied: {position[0] * position[1]}");
+        }
+
+        private static void First()
+        {
+            int[] position = new int[2];
+
+            foreach (string line in File.ReadLines(@"input.txt"))
+            {
+                var command = line.Split(" ")[0];
+                var move = int.Parse(line.Split(" ")[1]);
+
+                if (command.Equals("forward"))
+                    position[0] += move;
+                if (command.Equals("down"))
+                    position[1] += move;
+                if (command.Equals("up"))
+                    position[1] -= move;
+            }
+
+            Console.WriteLine($"Final position is {position[0]} with depth {position[1]}. Multiplied: {position[0] * position[1]}");
+        }
+    }
+}
